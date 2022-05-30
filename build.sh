fb0412964e6dbe8ce76a0327618262b68eb048ae
tuxa <69870860+mrtuxa@users.noreply.github.com>
2022-05-30 10:25:33 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,14 +8,14 @@ gcc -m32 -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 
 # linking kernel with kernel.o and boot.o
 
-ld -m elf_i386 -T linker.ld kernel.o boot.o -o mrOS.bin -nostdlib
+ld -m elf_i386 -T linker.ld kernel.o boot.o -o FreaX.bin -nostdlib
 
 # check binary file is x86 multiboot or not
-grub-file --is-x86-multiboot mrOS.bin
+grub-file --is-x86-multiboot FreaX.bin
 
 # building the iso file
 
 mkdir out/boot/grub
-cp mrOS.bin out/boot -v
+cp FreaX.bin out/boot -v
 cp -v grub.cfg out/boot
-grub-mkrescue -o mrOS out
+grub-mkrescue -o FreaX out
